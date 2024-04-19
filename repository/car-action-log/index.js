a3f4b81de549c0ea5a00660bbf501522adf48f75
Shahrizan <shahrizan.code@gmail.com>
2024-04-19 12:47:37 +0700

    create car action log repo

--- repository/car-action-log/index.js
+++ repository/car-action-log/index.js
@@ -1,4 +1,67 @@
 const { randomUUID } = require('crypto');
 
-const path = require('path');
-const { }
+const { CarActionLog, Car, User } = require('../../models');
+const NotFoundError = require('../../exceptions/NotFoundError');
+
+const redisHelper = require('../../helper/redis');
+
+const getCarActionLogByCarId = async (carId) => {
+  try {
+    const result = await redisHelper.getData(`car-action-log:${carId}`);
+    return result;
+  } catch (error) {
+    const car = await Car.findByPk(carId);
+
+    if (!car) {
+      throw new NotFoundError('Car not found.');
+    }
+
+    const result = await CarActionLog.findAll({
+      where: {
+        car_id: carId,
+      },
+      include: [
+        {
+          model: User,
+        },
+        {
+          model: Car,
+        },
+      ],
+    });
+
+    await redisHelper.setData(`car-action-log:${carId}`, result);
+    return result;
+  }
+};
+
+const postCarActionLogByCarId = async (carId, userId, action) => {
+  const id = `car-action-log:${randomUUID()}`;
+
+  const car = await Car.findByPk(carId);
+
+  if (!car) {
+    throw new NotFoundError('Car not found.');
+  }
+
+  const user = await User.findByPk(userId);
+
+  if (!user) {
+    throw new NotFoundError('User not found.');
+  }
+
+  const result = await CarActionLog.create({
+    id,
+    car_id: carId,
+    user_id: userId,
+    action,
+  });
+
+  await redisHelper.setData(`car-action-log:${carId}`, result);
+  return result;
+};
+
+module.exports = {
+  getCarActionLogByCarId,
+  postCarActionLogByCarId,
+};
